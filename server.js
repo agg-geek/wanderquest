71cf555b101b757a50b29f0d4342429089f9f3de
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-14 20:59:05 +0530

    Use dotenv for env variables

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const app = require('./app');
 const dotenv = require('dotenv');
 
-console.log(app.get('env')); // development, env variable set by express
-console.log(process.env); // env variables set by nodejs
-// many packages depend on NODE_ENV, which is not defined by express
-// define it as 'NODE_ENV=development nodemon server.js' in the terminal
-// define other variables like 'X=23 nodemon server.js' if required
+// you might otherwise use config.env as defining env variable in the terminal is not very nice
+// use dotenv to connect to that env file
+dotenv.config({ path: './config.env' });
 
-const port = 3000;
+// keep this log here to see the env variables read by dotenv in above line
+console.log(process.env);
+
+// also keep this app after reading dotenv so that
+// the process.env variables become available to code in app.js and stuff
+const app = require('./app');
+
+// also have a look at the package.json files
+
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
 	console.log(`Server is listening on port ${port}`);
 });
