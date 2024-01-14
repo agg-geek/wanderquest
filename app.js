41ca2000a3bc95c536daf38d04192b4a27328910
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-14 17:02:32 +0530

    Update tour using PATCH request

--- app.js
+++ app.js
@@ -15,14 +15,7 @@ app.get('/api/v1/tours', (req, res) => {
 	});
 });
 
-// '/api/v1/tours/:id/:x/:y' need to specify id, x, y
-// if you didn't specify y, then error (as the exact URL is not matched)
-// to keep y optional, use /tours/:id/:x/:y?
-// for which req.params gives { id: '4', x: '20', y: undefined }
 app.get('/api/v1/tours/:id', (req, res) => {
-	// console.log(req.params);
-
-	// req.params.id is a string, but tour.id is Number
 	const tourId = Number(req.params.id);
 	const tour = tours.find(tour => tour.id === tourId);
 
@@ -49,6 +42,25 @@ app.post('/api/v1/tours', (req, res) => {
 			status: 'success',
 			data: { newTour },
 		});
+	});
+});
+
+// put - update complete data, patch - update changed data
+// dummy implementation:
+app.patch('/api/v1/tours/:id', (req, res) => {
+	const tourId = Number(req.params.id);
+	if (tourId > tours.length) {
+		return res.status(404).json({
+			status: 'fail',
+			message: 'Invalid ID',
+		});
+	}
+
+	res.status(200).json({
+		status: 'success',
+		data: {
+			tour: '<Updated tour here...>',
+		},
 	});
 });
 
