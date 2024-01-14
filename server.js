66d9f07f1472bf97c5c32cad9db886fbf7edd550
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-14 23:56:00 +0530

    Connect to MongoDB Atlas using mongoose

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+mongoose
+	.connect(process.env.DATABASE_URL)
+	.then(() => console.log('Successfully connected to the database!'))
+	.catch(() => console.log('Connection to database failed'));
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
