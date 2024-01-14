1cfd7d088f6b658df74e666749ae2f4b2ad874f8
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-14 20:33:12 +0530

    Add middleware to serve static files

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ app.use(morgan('dev'));
 
 app.use(express.json());
 
+// serving static files from public folder
+// if file is in /public/img/image.png, then
+// /img/image.png is how the file will be served and not /public/img/image.png
+// also note /img/ path will not be served as express considers it to be a normal route
+// and route /img/ has not been implemented
+app.use(express.static(`${__dirname}/public`));
+
 app.use((req, res, next) => {
 	req.requestTime = new Date().toISOString();
 	next();
