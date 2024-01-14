bb2e345c7033269b65a96e0af079447a18c18a87
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-14 16:42:04 +0530

    Create tour using POST route

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
 const express = require('express');
+
 const app = express();
+
+// the data from post is not directly added to req obj by express
+// we need to use express.json() (a middleware) to do that
+// without this, req.body will be undefined for a post requeest (checked!)
+app.use(express.json());
 
 // notice JSON.parse and readFileSync (sync, but at the top level)
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-files/data/tours-simple.json`));
@@ -12,6 +18,24 @@ app.get('/api/v1/tours', (req, res) => {
 		status: 'success',
 		results: tours.length,
 		data: { tours },
+	});
+});
+
+// send the post request with body in raw/json format
+app.post('/api/v1/tours', (req, res) => {
+	// console.log(req.body);
+
+	const tourId = tours.at(-1).id + 1;
+	const newTour = Object.assign({ id: tourId }, req.body); // req.body.id = tourId mutates req.body
+	tours.push(newTour);
+
+	// notice JSON.stringify(tours)
+	fs.writeFile(`${__dirname}/dev-files/data/tours-simple.json`, JSON.stringify(tours), err => {
+		res.status(201).json({
+			// status 201 means created
+			status: 'success',
+			data: { newTour },
+		});
 	});
 });
 
