142761ae03e5efad8b5309a008df1e291b2595a2
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-14 20:50:24 +0530

    Display env variables

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const app = require('./app');
+const dotenv = require('dotenv');
+
+console.log(app.get('env')); // development, env variable set by express
+console.log(process.env); // env variables set by nodejs
+// many packages depend on NODE_ENV, which is not defined by express
+// define it as 'NODE_ENV=development nodemon server.js' in the terminal
+// define other variables like 'X=23 nodemon server.js' if required
 
 const port = 3000;
 app.listen(port, () => {
