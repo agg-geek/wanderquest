b75e9d0cf05b8220d67aefb6f867c913dc704b07
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-15 10:07:44 +0530

    Update tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,15 +5,52 @@ const tourSchema = new mongoose.Schema({
 		type: String,
 		required: [true, 'A tour must have a name'],
 		unique: true,
+		trim: true,
 	},
 	price: {
 		type: Number,
 		required: [true, 'A tour must have a price'],
 	},
-	rating: {
+	duration: {
+		type: Number,
+		required: [true, 'A tour must have a duration'],
+	},
+	maxGroupSize: {
+		type: Number,
+		required: [true, 'A tour must have a max group size'],
+	},
+	difficulty: {
+		type: String,
+		required: [true, 'A tour must have a difficulty'],
+	},
+	ratingsAvg: {
 		type: Number,
 		default: 4,
 	},
+	ratingsQuantity: {
+		type: Number,
+		default: 0,
+	},
+	priceDiscount: Number,
+	summary: {
+		type: String,
+		required: [true, 'A tour must have a summary'],
+		trim: true,
+	},
+	description: {
+		type: String,
+		trim: true,
+	},
+	imageCover: {
+		type: String,
+		required: [true, 'A tour must have a cover image'],
+	},
+	images: [String],
+	createdAt: {
+		type: Date,
+		default: Date.now(),
+	},
+	startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
