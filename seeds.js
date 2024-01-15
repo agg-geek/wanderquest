e6b0e4bae7b30adccc25d2267c0e040188e65d93
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-15 13:19:43 +0530

    Seed database

---
+++ seeds.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
+const Tour = require('./models/tourModel');
+
+mongoose
+	.connect(process.env.DATABASE_URL)
+	.then(() => console.log('Successfully connected to the database!'))
+	.catch(() => console.log('Connection to database failed'));
+
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-files/data/tours-simple.json`, 'utf-8'));
+
+const deleteTours = async () => {
+	try {
+		await Tour.deleteMany();
+		console.log('Tours successfully deleted');
+		process.exit();
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+const importTours = async () => {
+	try {
+		await Tour.create(tours);
+		console.log('Tours successfully saved');
+		process.exit();
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+// console.log(process.argv);
+
+if (process.argv[2] === '--importT') importTours();
+else if (process.argv[2] === '--deleteT') deleteTours();
