ea14c03d06ba50d656759320ef037d17cd2a2197
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-16 12:28:08 +0530

    Create error handling middleware

--- app.js
+++ app.js
@@ -17,12 +17,25 @@ app.use(express.static(`${__dirname}/public`));
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-// notice app.all for all HTTP verbs and * for all urls
-// also handle this route after all the above routes
-app.all('*', (req, res) => {
-	res.status(404).json({
-		status: 'fail',
-		message: `Cannot find ${req.originalUrl} on this server`,
+app.all('*', (req, res, next) => {
+	const err = new Error(`Cannot find ${req.originalUrl} on this server`); // this is err.message
+	err.status = 'fail';
+	err.statusCode = 404;
+	// whenever we pass any argument into next, express will automatically consider it as an error
+	// express will skip any normal middleware in next() and straight away go to the error handling middleware
+	next(err);
+});
+
+// express will recognise this as error handling middleware
+// as it consists of 4 params
+// so this is only called when an error has occurred
+app.use((err, req, res, next) => {
+	err.statusCode = err.statusCode || 500;
+	err.status = err.status || 'error';
+
+	res.status(err.statusCode).json({
+		status: err.status,
+		message: err.message,
 	});
 });
 
