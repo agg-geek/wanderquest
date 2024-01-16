41c272a62958ff5ae6c1ee0325d5337181bf796b
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-16 12:15:20 +0530

    Handle 404 routes

--- app.js
+++ app.js
@@ -17,4 +17,13 @@ app.use(express.static(`${__dirname}/public`));
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// notice app.all for all HTTP verbs and * for all urls
+// also handle this route after all the above routes
+app.all('*', (req, res) => {
+	res.status(404).json({
+		status: 'fail',
+		message: `Cannot find ${req.originalUrl} on this server`,
+	});
+});
+
 module.exports = app;
