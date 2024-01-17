7c1c5366506b06fa274832aa804c003f0f904a0a
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-17 12:28:00 +0530

    Close server and then exit app on promise rejection

--- server.js
+++ server.js
@@ -8,25 +8,20 @@ mongoose
 	.connect(process.env.DATABASE_URL)
 	.then(() => console.log('Successfully connected to the database!'));
 // .catch(() => console.log('Connection to database failed'));
-// if this catch block is removed and the mongodb password in config.env is incorrect,
-// we get auth error which is an unhandled promise rejection (as catch block is removed)
-// this error is not an express error, and such promise rejections can happen anywhere
-// here, we can fix it by just adding the catch block
-// however, we handle promise rejections globally
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
+const server = app.listen(port, () => {
 	console.log(`Server is listening on port ${port}`);
 });
-
-// whenever there is unhandled promise rejection anywhere in our app,
-// process will emit an event called unhandled rejection and we can subscribe to that event
 
 process.on('unhandledRejection', err => {
 	console.log(err.name); // MongoServerError
 	console.log(err.message); // bad auth
 	console.log('Unhandled promise rejection, shutting down');
-
-	// we can just shutdown the app in this case, using process.exit()
-	process.exit(1);
+	// process.exit exists the app abruptly, all ongoing requests are just stopped
+	// we stop more gracefully by closing the server which finishes ongoing requests first
+	// and after this, the callback runs and we shut the app
+	server.close(() => {
+		process.exit(1);
+	});
 });
