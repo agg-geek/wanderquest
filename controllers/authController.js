3c8619d29d3282ffb40135b82585a4f387682ba0
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-17 15:55:56 +0530

    Fix security flaw in signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,7 +2,16 @@ const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 
 module.exports.signup = catchAsync(async (req, res, next) => {
-	const newUser = await User.create(req.body);
+	// using the complete req.body has a serious security flaw
+	// like a user may state his role: 'admin' (which we will define in the model later)
+	// and thus become a user
+	const newUser = await User.create({
+		name: req.body.name,
+		email: req.body.email,
+		password: req.body.password,
+		passwordConfirm: req.body.passwordConfirm,
+	});
+
 	res.status(201).json({
 		status: 'success',
 		data: { user: newUser },
