2e747603a933cffb19ee8c243eefad9f4e3246b4
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-17 11:35:26 +0530

    Handle MongoDB cast error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,12 @@
+const AppError = require('./../utils/appError');
+
+const handleDBCastError = err => {
+	// err contains path: '_id' which is the variable with wrong objectid
+	// err also contains value as 'www' which is the invalid objectid
+	const message = `Invalid ${err.path}: ${err.value}`;
+	return new AppError(message, 400);
+};
+
 const sendDevError = (err, res) => {
 	res.status(err.statusCode).json({
 		status: err.status,
@@ -9,13 +18,11 @@ const sendDevError = (err, res) => {
 
 const sendProdError = (err, res) => {
 	if (err.isOperational) {
-		// Our custom errors, trustable, can send this to the client
 		res.status(err.statusCode).json({
 			status: err.status,
 			message: err.message,
 		});
 	} else {
-		// some unexpected programming error / bug, don't send to client
 		console.error(err);
 		res.status(err.statusCode).json({
 			status: '500',
@@ -31,6 +38,20 @@ module.exports = (err, req, res, next) => {
 	if (process.env.NODE_ENV === 'development') {
 		sendDevError(err, res);
 	} else {
+		// for production, we separate the errors into operational and non-operational
+		// the errors we generate ourselves are operational errors, which have been generated using AppError
+		// because we trust these operational errors, we show more meaningful info to the client for these errors
+		// however, there are some other errors created by mongoose/mongodb which we can handle (can trust) but they are not operational
+		// they aren't operational simply because we didn't create them and they don't have isOperational
+		// hence we simply show 500 status 'Something went wrong'
+		// however, we handle them here
+		// see the exact error through 'error: err' in dev mode (clg in prod mode doesn't show the err)
+		// there are multiple such errors, which we handle one by one:
+
+		// 1. getTour with id 'www', this is an invalid mongodb ObjectId
+		// (there is difference bw invalid mongodb ObjectId and valid objectid for which document does not exist)
+		// this error (as seen through dev mode error: err) has name CastError, and is handled below
+		if (err.name === 'CastError') err = handleDBCastError(err);
 		sendProdError(err, res);
 	}
 };
