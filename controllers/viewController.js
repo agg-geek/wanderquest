70b9aba79d1425ddf0c3981e51c783f75e94da49
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-22 12:57:17 +0530

    Add root route

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,7 +1,7 @@
 const Tour = require('./../models/tourModel');
 const catchAsync = require('./../utils/catchAsync');
 
-module.exports.getOverview = catchAsync(async (req, res, next) => {
+module.exports.getAllTours = catchAsync(async (req, res, next) => {
 	const tours = await Tour.find();
 	res.status(200).render('tours/index', {
 		title: 'All tours',
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -3,6 +3,10 @@ const viewController = require('./../controllers/viewController');
 
 const router = express.Router();
 
-router.get('/', viewController.getOverview);
+router.get('/', (req, res) => {
+	res.status(200).render('home', { title: 'Home' });
+});
+
+router.get('/tours', viewController.getAllTours);
 
 module.exports = router;
