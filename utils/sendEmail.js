8865613cb2f1d96d494074d83821b0391d1df98e
Abhinav Aggarwal <92445847+agg-geek@users.noreply.github.com>
2024-01-24 19:27:10 +0530

    Create Email class

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,24 +1,59 @@
 const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async options => {
-	const transporter = nodemailer.createTransport({
-		host: process.env.EMAIL_HOST,
-		port: process.env.EMAIL_PORT,
-		auth: {
-			user: process.env.EMAIL_USERNAME,
-			pass: process.env.EMAIL_PASSWORD,
-		},
-	});
+class Email {
+	constructor(user, url) {
+		this.to = user.email;
+		this.firstName = user.name.split(' ')[0];
+		this.url = url;
+		this.from = `Abhinav Aggarwal <${process.env.EMAIL_FROM}>`;
+	}
 
-	const mailOptions = {
-		from: 'Abhinav Aggarwal <abhiagg@aggeek.io>',
-		to: options.email,
-		subject: options.subject,
-		text: options.message,
-	};
+	_newTransport() {
+		if (process.env.NODE_ENV === 'production') {
+			// TODO: Sendgrid
+			return;
+		}
 
-	await transporter.sendMail(mailOptions);
-	console.log('sent mail');
-};
+		return nodemailer.createTransport({
+			host: process.env.EMAIL_HOST,
+			port: process.env.EMAIL_PORT,
+			auth: {
+				user: process.env.EMAIL_USERNAME,
+				pass: process.env.EMAIL_PASSWORD,
+			},
+		});
+	}
 
-module.exports = sendEmail;
+	async send(template, subject) {
+		const html = ejs.render(`${__dirname}/../views/email/${template}.ejs`, {
+			firstName: this.firstName,
+			url: this.url,
+			subject,
+		});
+
+		const mailOptions = {
+			from: this.from,
+			to: this.to,
+			subject,
+			html,
+			text: htmlToText.fromString(html),
+		};
+
+		await this._newTransport().sendMail(mailOptions);
+	}
+
+	async sendWelcome() {
+		await this.send('welcome', 'Welcome to the Natours Family!');
+	}
+
+	async sendPasswordReset() {
+		await this.send(
+			'passwordReset',
+			'Your password reset token (valid for only 10 minutes)'
+		);
+	}
+}
+
+module.exports = Email;
